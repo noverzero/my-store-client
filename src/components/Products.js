f3927da57ded8d465cad4bd071747b602b8c7857
Matt Winzer <matt.winzer@gmail.com>
2019-07-02 15:42:49 -0600

    added react router, created basic Nav, Home, and Products components

---
+++ src/components/Products.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Products = () => {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>Welcome to Products List!</h1>
+      </header>
+    </div>
+  )
+}
+
+export default Products
