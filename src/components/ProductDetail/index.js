75f0ea7449cd064f39c335baa57a4a2010de8d76
Matt Winzer <matt.winzer@gmail.com>
2019-07-11 15:18:01 -0600

    added stripe functionality with test data

--- src/components/ProductDetail/index.js
+++ src/components/ProductDetail/index.js
@@ -24,6 +24,30 @@ class ProductDetail extends Component {
       })
   }
 
+  initiateStripeCheckout = async () => {
+    const stripe = window.Stripe('pk_test_U97OqImzWdzzRotfPzXt3Req00UQtQ1rnD')
+
+    try {
+      // Initiate checkout session to get session id
+      const response = await fetch('http://localhost:4000/api/checkout', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+          'Accept': 'application/json'
+        }
+      })
+
+      const data = await response.json()
+      const sessionId = data.session.id
+
+      // Redirect to checkout
+      const result = await stripe.redirectToCheckout({ sessionId })
+
+    } catch (error) {
+      console.log('STRIPE ERROR', error)
+    }
+  }
+
   render() {
     return (
       <div>
@@ -33,6 +57,7 @@ class ProductDetail extends Component {
           <img src={this.state.product.img_url} alt="product" />
           <p>{this.state.product.description}</p>
           <h3>Price: ${this.state.product.price / 100}.00</h3>
+          <button onClick={this.initiateStripeCheckout}>Purchase</button>
         </Card>
       </div>
     )
