8d52be2598fb2d1c2617f9235d7c4e5d146adc6d
Matt Winzer <matt.winzer@gmail.com>
2019-07-12 18:07:44 -0600

    removed Loader from Home page

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,13 +1,10 @@
 import React from 'react'
 
-import Loader from './Loader'
-
 const Home = () => {
   return (
     <div className="App">
       <header className="App-header">
         <h1>Welcome Home!</h1>
-        <Loader />
       </header>
     </div>
   )
