164a7ce9a96c50bbbc538cbf2c6a00137fcc6ea0
dustinhuth <dustin@undefinedindustries.com>
2019-07-18 14:24:29 -0600

    auth integrated

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -1,11 +1,32 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
+import { useAuth0 } from '../react-auth0-wrapper';
+
 
 const Navigation = () => {
+  const { isAuthenticated, loginWithRedirect, logout } = useAuth0();
+
   return (
     <nav>
       <Link to='/'>Home</Link>
       <Link to='/products'>Products</Link>
+      {/* NEW - add a link to the home and profile pages */}
+      {isAuthenticated && (
+        <span>
+          <Link to="/profile">Profile</Link>
+        </span>
+      )}
+      {!isAuthenticated && (
+        <button
+          onClick={() =>
+            loginWithRedirect({})
+          }
+        >
+          Log in
+        </button>
+      )}
+
+      {isAuthenticated && <button onClick={() => logout()}>Log out</button>}
     </nav>
   )
 }
